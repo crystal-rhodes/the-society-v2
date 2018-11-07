3d55cfbde8992fc2776d3db6305b4e178131aad7
Your Name <you@example.com>
2018-11-07 11:27:29 -0500

    deleted server and added styles

--- client/src/app.js
+++ client/src/app.js
@@ -5,7 +5,7 @@ import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { login, logout } from './actions/auth';
 import 'normalize.css/normalize.css';
-import './styles/styles.scss';
+import './styles/main.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from './firebase/firebase';
 import LoadingPage from './components/LoadingPage';
